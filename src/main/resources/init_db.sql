2bc6de2b288e66b669525f2bee761b14a83cad07
LinkClink <shekaqwe@gmail.com>
2022-04-15 12:29:06 +0300

    Create init_db.sql

---
+++ src/main/resources/init_db.sql
@@ -0,0 +1,14 @@
+CREATE SCHEMA IF NOT EXISTS `chat` DEFAULT CHARACTER SET utf8;
+USE `chat`;
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+DROP TABLE IF EXISTS `messages`;
+CREATE TABLE `messages`  (
+                            `id` bigint(0) UNSIGNED NOT NULL AUTO_INCREMENT,
+                            `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+                            `content` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+                            `send_time` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+                            PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
